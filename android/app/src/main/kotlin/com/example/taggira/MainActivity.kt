3db2c7bae0c756e220609602d5abff756d5396f7
Mostafa Qurany <79308055+MostafaQurany@users.noreply.github.com>
2025-04-20 13:55:09 +0200

    set up the app

---
+++ android/app/src/main/kotlin/com/example/taggira/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.taggira
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app/taggira.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:taggira/main.dart';
+
+class Taagira extends StatelessWidget {
+  const Taagira({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
